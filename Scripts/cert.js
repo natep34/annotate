3a1e6f7df0571e7316041c8f19fbb2c78dbbf5bc
Nate Phillips <nate.l.p@gmail.com>
2013-05-22 19:27:09 -0700

    more styling changes

--- Scripts/cert.js
+++ Scripts/cert.js
@@ -2,7 +2,7 @@ if (Meteor.isClient){
 
 	Template.cert.rendered = function(){
 		$(".popped").popover().click(function(e){e.preventDefault();});
-		$(".home-btn").click(function(){
+		$(".brand").click(function(){
 			Meteor.Router.to('/');
 			$('body').scrollTop(0);
 		});
